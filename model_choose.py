7203aad520ab8eb859188c007d14bb6181ca7aea
Mona <lixinqi98@gmail.com>
2022-05-30 15:53:21 +0200

    kernel size

--- model_choose.py
+++ model_choose.py
@@ -3,6 +3,9 @@ from Resnet50_v2 import resnet50_v2
 from Resnet50_v3 import resnet50_v3
 from Resnet50_v4 import resnet50_v4
 from Resnet50_v5 import resnet50_v5
+from Resnet50_v6 import resnet50_v6
+from Resnet50_v7 import resnet50_v7
+from Resnet50_v8 import resnet50_v8
 
 def model_choose(model_name="ResNet_v1", pretrained: bool = False):
     """
@@ -19,5 +22,10 @@ def model_choose(model_name="ResNet_v1", pretrained: bool = False):
         model = resnet50_v4(pretrained)
     if model_name == "ResNet_v5":
         model = resnet50_v5(pretrained)
-
+    if model_name == "ResNet_v6":
+        model = resnet50_v6(pretrained)
+    if model_name == "ResNet_v7":
+        model = resnet50_v7(pretrained)
+    if model_name == "ResNet_v8":
+        model = resnet50_v8(pretrained)
     return model
