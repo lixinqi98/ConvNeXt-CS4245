0df50717ca3e28dce76a3d48ce05d86a18a6b6d8
wanghaoran131 <wanghaoran131@gmail.com>
2022-06-08 11:29:01 +0200

    ReLU -> GELU

--- Resnet50_v11.py
+++ Resnet50_v11.py
@@ -64,7 +64,7 @@ class BasicBlock(nn.Module):
         # Both self.conv1 and self.downsample layers downsample the input when stride != 1
         self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = norm_layer(planes)
-        self.gelu = nn.GELU(inplace=True)
+        self.gelu = nn.GELU()
         self.conv2 = conv3x3(planes, planes)
         self.bn2 = norm_layer(planes)
         self.downsample = downsample
